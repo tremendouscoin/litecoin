4421eb08b2b04703aa9a268e4d472211e13c4826
patekuru <patrick@limozipcorp.com>
2018-01-24 09:44:58 -0500

    update chainparams.cpp. activate BIP34, BIP65, and BIP66 from the 0th block, why not?, kind of as per https://bitcoin.stackexchange.com/questions/54219/how-to-calculate-the-bip34hash

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -71,10 +71,10 @@ public:
     CMainParams() {
         strNetworkID = "main";
         consensus.nSubsidyHalvingInterval = 840000;
-        consensus.BIP34Height = 710000;
-        consensus.BIP34Hash = uint256S("fa09d204a83a768ed5a7c8d441fa62f2043abf420cff1226c7b4329aeb9d51cf");
-        consensus.BIP65Height = 918684; // bab3041e8977e0dc3eeff63fe707b92bde1dd449d8efafb248c27c8264cc311a
-        consensus.BIP66Height = 811879; // 7aceee012833fa8952f8835d8b1b3ae233cd6ab08fdb27a771d2bd7bdc491894
+        consensus.BIP34Height = 0;
+        consensus.BIP34Hash = uint256S("3e55bee2eaab8b99f8a07e1a9120ed42e404068e7479cd477e9a5aa5d301cc20");
+        consensus.BIP65Height = 0;
+        consensus.BIP66Height = 0;
         consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 3.5 * 24 * 60 * 60; // 3.5 days
         consensus.nPowTargetSpacing = 2.5 * 60;
