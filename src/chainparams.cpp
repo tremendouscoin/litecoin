ed4aa37cda357e7ec33b69a1fd7748ecc64d52dc
patekuru <patrick@limozipcorp.com>
2018-01-24 09:25:20 -0500

    update chainparams.cpp. nMinimumChainWork should probably be 0x00 since there isn't a chain yet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -97,7 +97,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1517356801; // January 31st, 2018
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000006805c7318ce2736c0");
+        consensus.nMinimumChainWork = uint256S("0x00");
 
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x1673fa904a93848eca83d5ca82c7af974511a7e640e22edc2976420744f2e56a"); //1155631
